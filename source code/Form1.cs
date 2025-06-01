41a5dd9bdfb0746fab4959061078c03c11d7175f
MalwareMaster <platformercatgamercat@gmail.com>
2025-06-01 00:47:54 -0700

    Create Form1.cs

---
+++ source code/Form1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Windows.Forms;
+
+namespace Lilbrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Lilbrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            // Your logic here
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Lilbrowser1.CanGoBack)
+                Lilbrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Lilbrowser1.CanGoForward)
+                Lilbrowser1.GoForward();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Lilbrowser1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Lilbrowser1.Navigate("https://www.google.com");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Lilbrowser1.Navigate(addressBox.Text);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Lilbrowser1.Navigate("https://www.google.com");
+        }
+    }
+}
